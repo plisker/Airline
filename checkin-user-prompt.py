a9b71a24eb0f3e18cae82e0fcde0b0cd20d075ac
Paul Lisker <plisker@gmail.com>
2018-12-03 02:32:12 -0500

    Adhere to Python style as much as possible

--- checkin-user-prompt.py
+++ checkin-user-prompt.py
@@ -1,9 +1,10 @@
 import checkin
 import sys
 
-################################################################################
+###############################################################################
 # Do not modify anything
-################################################################################
+###############################################################################
+
 
 def main():
     while True:
@@ -19,38 +20,40 @@ def main():
 
     print("What is your last name?")
     lastName = checkin.get_input()
-    
+
     while True:
         print("What is the day of your departure?")
         departure_day = checkin.get_input()
         departure_day = int(departure_day)
-        if departure_day not in range(1,32):
+        if departure_day not in range(1, 32):
             print("That is not a valid day of the month")
         else:
             break
-    
+
     while True:
         print("What is the month of your departure?")
         departure_month = checkin.get_input()
         departure_month = int(departure_month)
-        if departure_month not in range(1,13):
+        if departure_month not in range(1, 13):
             print("That is not a valid day of the month")
         else:
             break
-    
+
     print("What is the year of your departure?")
-    departure_year = checkin.get_input() # 4 digits
+    departure_year = checkin.get_input()  # 4 digits
     departure_year = int(departure_year)
-    
+
     while True:
-        print("What is the hour of your departure? (Not full time, just the hour.) Please use 24-hour format.")
-        departure_hour = checkin.get_input() # 24 hour format
+        print("What is the hour of your departure? (Not full time, " +
+              "just the hour.) Please use 24-hour format.")
+        departure_hour = checkin.get_input()  # 24 hour format
         departure_hour = int(departure_hour)
         if departure_hour not in range(25):
-            print("That is not a valid hour. Remember that midnight is simply 0.")
+            print("That is not a valid hour. " +
+                  "Remember that midnight is simply 0.")
         else:
             break
-    
+
     while True:
         print("What is the minute of your departure?")
         departure_minute = checkin.get_input()
@@ -61,9 +64,10 @@ def main():
             break
     print("Thank you! Loading . . .")
 
-    return checkin.main(confirmationNumber, firstName, lastName, departure_day, departure_month, departure_year, departure_hour, departure_minute)
+    return checkin.main(confirmationNumber, firstName, lastName, departure_day,
+                        departure_month, departure_year, departure_hour,
+                        departure_minute)
+
 
 if __name__ == "__main__":
     browser = main()
-
-
--- checkin.py
+++ checkin.py
@@ -6,10 +6,10 @@ from datetime import datetime, timedelta
 from threading import Timer
 
 # Do not modify anything above this line
-################################################################################
+###############################################################################
 # Enter personal info here!
 
-confirmationNumber = 'ABC123' 
+confirmationNumber = 'ABC123'
 firstName = 'PAUL'
 lastName = 'LISKER'
 departure_day = 16
@@ -18,11 +18,12 @@ departure_year = 2016   # 4 digits
 departure_hour = 18     # 24 hour format
 departure_minute = 50
 
-################################################################################
+###############################################################################
 # Do not modify anything below this line
 
+
 def checkin(browser):
-    time.sleep(3) # To make sure that their server allows for checking in.
+    time.sleep(3)  # To make sure that their server allows for checking in.
     checkin_button = browser.find_by_id('form-mixin--submit-button')
     checkin_button.click()
     time.sleep(3)
@@ -33,6 +34,7 @@ def checkin(browser):
     quit(browser)
     return
 
+
 def get_input():
     get_user_input = input
 
@@ -42,24 +44,28 @@ def get_input():
 
     return get_user_input()
 
+
 def quit(browser):
     print("When you're finished, click enter to close the browser session.")
     get_input()
     browser.quit()
 
-def main(confirmationNumber, firstName, lastName, departure_day, departure_month, departure_year, departure_hour, departure_minute):
+
+def main(confirmationNumber, firstName, lastName, departure_day,
+         departure_month, departure_year, departure_hour, departure_minute):
     browser = sp.Browser('chrome')
     browser.visit('https://www.southwest.com/air/check-in/index.html')
-    conf_num_field = browser.find_by_id('confirmationNumber').fill(confirmationNumber)
+    conf_num_field = browser.find_by_id('confirmationNumber').fill(
+        confirmationNumber)
     first_name_field = browser.find_by_id('passengerFirstName').fill(firstName)
     last_name_field = browser.find_by_id('passengerLastName').fill(lastName)
 
     departure = datetime(departure_year,
-                    departure_month,
-                    departure_day,
-                    departure_hour,
-                    departure_minute, 1)
-    checkin_time = departure - timedelta(days = 1)
+                         departure_month,
+                         departure_day,
+                         departure_hour,
+                         departure_minute, 1)
+    checkin_time = departure - timedelta(days=1)
     now = datetime.now()
     delta_t = checkin_time - now
     secs = delta_t.seconds + 1
@@ -72,11 +78,14 @@ def main(confirmationNumber, firstName, lastName, departure_day, departure_month
     else:
         days, hours, minutes, seconds = delta_t.days, delta_t.seconds // 3600, delta_t.seconds // 60 % 60, delta_t.seconds % 60
         if (days > 0):
-            print('Waiting', days, 'days,', hours, 'hours,', minutes, 'minutes, and', seconds, 'seconds before checking in...')
+            print('Waiting', days, 'days,', hours, 'hours,', minutes,
+                  'minutes, and', seconds, 'seconds before checking in...')
         elif (hours > 0):
-            print('Waiting', hours, 'hours,', minutes, 'minutes, and', seconds, 'seconds before checking in...')
+            print('Waiting', hours, 'hours,', minutes, 'minutes, and',
+                  seconds, 'seconds before checking in...')
         elif (minutes > 0):
-            print('Waiting', minutes, 'minutes and', seconds, 'seconds before checking in...')
+            print('Waiting', minutes, 'minutes and',
+                  seconds, 'seconds before checking in...')
         else:
             print('Waiting', seconds, 'seconds before checking in...')
 
@@ -84,7 +93,7 @@ def main(confirmationNumber, firstName, lastName, departure_day, departure_month
         t.start()
 
     return browser
-    
+
 
 if __name__ == "__main__":
     # Support Python 2 and 3 input
@@ -94,10 +103,11 @@ if __name__ == "__main__":
     # If this is Python 2, use raw_input()
     if sys.version_info[:2] <= (2, 7):
         get_input = raw_input
-    
-    browser = main(confirmationNumber, firstName, lastName, departure_day, departure_month, departure_year, departure_hour, departure_minute)
+
+    browser = main(confirmationNumber, firstName, lastName, departure_day,
+                   departure_month, departure_year, departure_hour,
+                   departure_minute)
 
     print("When you're finished, click enter to close the browser session.")
     get_input()
     browser.quit()
-
